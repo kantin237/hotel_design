7ff49e4af73f001b6c0ccf85244d85a30800284d
kantin237 <fomekongorich@yahoo.com>
2020-07-10 20:10:01 +0200

    first commit for hotel front-end

---
+++ src/components/FeaturedRooms.js
@@ -0,0 +1,34 @@
+import React , { Component } from "react";
+import {RoomContext} from "../context";
+import Loading from "./Loading";
+import Room from './Room';
+import Title from "./Title";
+
+export default class FeaturedRooms extends Component {
+    static contextType = RoomContext;
+    render() {
+        let { loading, featuredRooms: rooms } = this.context;
+        console.log(rooms);
+       rooms = [].map(room => {
+           return <Room key={room.id} room={room} />
+       });
+       
+        return (
+            <section className="featured-rooms">
+                <Title title="featured rooms" />
+                <div className="featured-rooms-center">
+                   {loading ? <Loading /> : rooms} 
+                </div>
+                <Loading />
+                <Room />
+            </section>
+        );
+       /* return (
+            <div>
+                <Room />
+                <Title title="featured rooms" />
+                <Loading />
+            </div>
+        );*/
+    }
+}
